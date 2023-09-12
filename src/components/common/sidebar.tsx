c42161f681e17e33f1c38f36197d09838a17f526
Paul Emas <pjobiggi@gmail.com>
2023-09-12 15:51:48 +0100

    feat: added link to home

--- src/components/common/sidebar.tsx
+++ src/components/common/sidebar.tsx
@@ -3,6 +3,7 @@ import { cn } from '@/lib/utils';
 import { Button } from '../ui/button';
 import { ScrollArea } from '../ui/scroll-area';
 import { useAppSelector } from '@/lib/redux/hooks';
+import Link from 'next/link';
 
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
   playlists: string[];
@@ -23,22 +24,24 @@ const Sidebar = ({ className, playlists }: SidebarProps) => {
         <div className="px-3 py-2">
           <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">Discover</h2>
           <div className="space-y-1">
-            <Button disabled={loading} variant="secondary" className="w-full justify-start">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <circle cx="12" cy="12" r="10" />
-                <polygon points="10 8 16 12 10 16 10 8" />
-              </svg>
-              Listen Now
-            </Button>
+            <Link href="/">
+              <Button disabled={loading} variant="secondary" className="w-full justify-start">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="mr-2 h-4 w-4"
+                >
+                  <circle cx="12" cy="12" r="10" />
+                  <polygon points="10 8 16 12 10 16 10 8" />
+                </svg>
+                Home
+              </Button>
+            </Link>
             <Button disabled={loading} variant="ghost" className="w-full justify-start">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
